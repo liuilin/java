a4dabbe59e5de88ab510ab0abc5d039e3522bd91
liuqiang <liu1142865396@gmail.com>
2022-05-23 17:31:18 +0800

    feat: 修改类

--- design-pattern/src/main/java/com/liumulin/design/creational/abstractfactory/JavaCourseFactory.java
+++ design-pattern/src/main/java/com/liumulin/design/creational/abstractfactory/JavaCourseFactory.java
@@ -1,6 +1,8 @@
 package com.liumulin.design.creational.abstractfactory;
 
 /**
+ * Java 课程工厂
+ *
  * @author Daniel Liu 2019/9/21 17:45
  */
 public class JavaCourseFactory implements CourseFactory {
